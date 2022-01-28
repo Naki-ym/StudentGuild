a9551d49ff83d1859bdc9caab5c7403f6bddc889
naoki yamada <naki@LAPTOP-I9DD1CEG.localdomain>
2022-01-28 22:54:58 +0900

    UX向上

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   validates :content, {presence: true, length: {maximum: 200}}
   validates :user_id, {presence: true}
+
+  def user
+    return User.find_by(id: self.user_id)
+  end
 end
