940d2d5488617ced96a36e056a1dfbd759b898d7
naoki yamada <naki@LAPTOP-I9DD1CEG.localdomain>
2022-04-17 18:49:17 +0900

    グループチャットルーム作成機能追加

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -39,19 +39,41 @@ class ChatsController < ApplicationController
   def edit
   end
   def create_group
+    @users_id = params[:users]
     @room = Room.new(name: params[:name], caption: params[:caption], is_group_chat: true)
-    unless @room.save
-      render("chats/create")
-      exit
-    end
-    @members = [@current_user, @user]
-    @members.each do |member|
-      @room_user = RoomUser.new(room_id: @room.id, user_id: member.id)
-      unless @room_user.save
-        render("chats/create")
+    if @room.save
+      @members = [@current_user]
+      @users_id.each do |user_id|
+        new_member = User.find_by(id: user_id, is_deleted: false)
+        @members << new_member
       end
+      @members.each do |member|
+        @room_user = RoomUser.new(room_id: @room.id, user_id: member.id)
+        unless @room_user.save
+          render("chats/create")
+        end
+      end
+      redirect_to("/chats/#{@room.id}")
+    else
+      @myroomusers = RoomUser.where(user_id: @current_user.id, is_deleted: false)
+      @rooms = []
+      @roomusers = []
+      @users = []
+      @myroomusers.each do |myroomuser|
+        @myroom = Room.find_by(id: myroomuser.room_id, is_group_chat: false, is_deleted: false)
+        @rooms << @myroom
+      end
+      @rooms.each do |room|
+        @roomuser = RoomUser.where.not(user_id: @current_user.id).find_by(room_id: room.id, is_deleted: false)
+        @roomusers << @roomuser
+      end
+      @roomusers.each do |roomuser|
+        @user = User.find_by(id: roomuser.user_id)
+        @users << @user
+      end
+      @error_message = "失敗しました"
+      render("chats/create")
     end
-    redirect_to("/chats/#{@room.id}")
   end
   def crate_individual
     @user = User.find_by(id: params[:id], is_deleted: false)
