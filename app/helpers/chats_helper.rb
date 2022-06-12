2958248dad831567b8202ab02e4324061fd5427f
naoki yamada <naki@LAPTOP-I9DD1CEG.localdomain>
2022-06-12 16:22:06 +0900

    チャットの最新のメッセージが表示されるよう変更

--- app/helpers/chats_helper.rb
+++ app/helpers/chats_helper.rb
@@ -1,2 +1,11 @@
 module ChatsHelper
+  def get_newest_message(room_id)
+    newest_message = Message.order(id: :desc).find_by(room_id: room_id, is_deleted: false)
+    if newest_message
+      content = newest_message.content
+      return content
+    else
+      return ""
+    end
+  end
 end
